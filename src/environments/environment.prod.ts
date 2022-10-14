d96d8da5d12002ba8c476bd333ef6c24c550aa4c
MariaShkrabo <shkrabo.mariya.2000.0473@gmail.com>
2022-10-14 09:36:21 +0300

    fix: change path to logo.svg

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,30 @@
+export const baseApiUrl: string = 'https://www.googleapis.com/youtube/v3/';
+
 export const environment = {
-  production: true,
+    production: true,
+    endpoints: {
+        commentThreads: {
+            getComments: `${baseApiUrl}commentThreads?part=snippet`,
+        },
+        channel: {
+            getChannel: `${baseApiUrl}channels?part=snippet,brandingSettings,statistics`,
+        },
+        channelSections: {
+            getChannelSections: `${baseApiUrl}channelSections?part=snippet,contentDetails`,
+        },
+        playlists: {
+            getPlaylists: `${baseApiUrl}playlists?maxResults=10&part=snippet,contentDetails`,
+            getPlaylistItems: `${baseApiUrl}playlistItems?part=contentDetails,snippet`,
+        },
+        search: {
+            getVideoBySearchingByKeyword: `${baseApiUrl}search?part=snippet&maxResults=25`,
+            getRelatedVideos: `${baseApiUrl}search?part=snippet&maxResults=25&type=video`,
+        },
+        videos: {
+            getVideo: `${baseApiUrl}videos?part=snippet,statistics`,
+        },
+        categories: {
+            getCategories: `${baseApiUrl}videoCategories?part=snippet&regionCode=BY`,
+        },
+    },
 };
